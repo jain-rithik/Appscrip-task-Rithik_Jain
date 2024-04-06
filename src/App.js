50bcff9ed664b552ac5ff57649a02091b6168474
Rithik <jainrithik2000@gmail.com>
2024-04-06 12:45:21 +0530

    body's title completed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Body from "./components/Body";
+import Body from "./components/Body/Body";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 
---
+++ src/components/Body/Title.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Title.css";
+
+const Title = () => {
+  return (
+    <div className="title-container">
+      <div className="title-content">
+        <h1>DISCOVER OUR PRODUCTS</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur. Amet est posuere rhoncus
+          scelerisque. Dolor integer scelerisque nibh amet mi ut elementum
+          dolor.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Title;
